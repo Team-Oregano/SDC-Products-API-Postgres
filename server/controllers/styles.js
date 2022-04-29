f1594007843ee44ca131c72aba64636162797da0
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-04-28 22:21:48 -0400

    perform local stress testing

--- server/controllers/styles.js
+++ server/controllers/styles.js
@@ -4,7 +4,6 @@ module.exports = {
   get: async (req, res) => {
     try {
       const data = await Model.getStyles(req.params.product_id)
-      console.log('data', data)
       res.status(200).send(data)
     } catch (err) {
       res.status(404).send(err)
